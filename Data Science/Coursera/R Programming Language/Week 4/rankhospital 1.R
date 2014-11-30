d58e5a4d505c32c68931a13a8d27764939d20f07
karundeepyadav <karundeepyadav@gmail.com>
2014-11-30 18:02:00 +0530

    Update rankhospital 1.R

--- Data Science/Coursera/R Programming Language/Week 4/rankhospital 1.R
+++ Data Science/Coursera/R Programming Language/Week 4/rankhospital 1.R
@@ -40,3 +40,50 @@ rankhospital <- function(state, outcome, num = "best") {
         
         sort(all.hospitals.with.current.rank)[1]
 }
+
+rankhospital1 <- function(state, outcome, num = "best") {
+        ## Read outcome data
+        data  <- read.csv("outcome-of-care-measures.csv", colClasses = "character",na.strings="Not Available")
+        
+        ## Check that state and outcome are valid        
+        validOutcome = c("heart attack","heart failure","pneumonia")
+        if (!outcome %in% validOutcome) { stop("invalid outcome")}
+        
+        validState = unique(data[,"State"])
+        if (!state %in% validState) stop("invalid state")
+        
+        ## convert outcome name into column name
+        fullColName <- c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
+        Column.Name <- fullColName[match(outcome,validOutcome)]
+        
+        ## Return hospital name in that state with the given rank
+        state.data  <- data[data$State == state,]
+        
+        final.Data  <- state.data[!(state.data[Column.Name] == "Not Available"),]
+        
+        final.Data  <- final.Data[!is.na(final.Data[Column.Name]),]
+        
+        tempData  <- final.Data[order(as.numeric(final.Data[[Column.Name]]),final.Data[["Hospital.Name"]],decreasing=FALSE,na.last=NA), ]                
+        
+        column.data  <- final.Data[,Column.Name]
+        
+        sorted.data  <- sort(column.data)
+        
+        uniqueValues  <- unique(as.numeric(sorted.data))
+        
+        rank  <- 1
+        
+        if (num == "best") rank  <- 1
+        else if (num == "worst") rank  <-length(uniqueValues)
+        else rank  <- as.integer(num)
+        
+        mortaility.for.given.rank  <- uniqueValues[rank]
+        
+        # Finding all hospitals with this mortaility rate
+        # all.hospitals.with.current.rank  <- final.Data[final.Data[Column.Name] == as.numeric(mortaility.for.given.rank),"Hospital.Name"]
+        #all.hospitals.with.current.rank  <- tempData[tempData[Column.Name] == as.numeric(mortaility.for.given.rank),"Hospital.Name"]
+        
+        
+        #sort(all.hospitals.with.current.rank)[1]
+        tempData[rank,"Hospital.Name"]
+}
