4b80ddf9a9932f4c31c1854c63abb893389844ed
karundeepyadav <karundeepyadav@gmail.com>
2014-11-30 07:46:36 +0530

    Best updated

---
+++ Data Science/Coursera/R Programming Language/Smart Commands.R
@@ -0,0 +1,12 @@
+# Here we are searching one variable into other vector
+# And below command shows how to do it quickly and correctly
+validOutcome = c("heart attack","heart failure","pneumonia")
+if (!outcome %in% validOutcome) { stop("invalid outcome")}
+
+# Getting unique values from a Column.
+# Here we populating validState from data present in myData dataset
+validState = unique(myData[,7])
+
+## convert outcome name into column name
+fullColName <- c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
+colName <- fullColName[match(outcome,validOutcome)]
